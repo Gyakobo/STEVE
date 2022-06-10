ad02836fd8b397d9bb259a15d81e215a74ef5a1c
Gyakobo <andygyakobo@gmail.com>
2022-06-10 01:56:12 -0400

    Just waged with scipy.signal find_peaks function

--- read_hdf5/remake_2.py
+++ read_hdf5/remake_2.py
@@ -2,29 +2,34 @@ import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 from   scipy.signal import find_peaks
+from   math import * 
 
 file_name = "data.hdf5"
+    
+def calc_alt(a, b, alpha):
+    return sqrt(a*a + b*b - 2*a*b*cos(alpha)) - a
 
-alt_name = [
-    "range=102.2", 
-    "range=138.17", 
-    "range=174.15", 
-    "range=210.12", 
-    "range=246.1", 
-    "range=282.07", 
-    "range=318.05", 
-    "range=354.02", 
-    "range=390.0", 
-    "range=425.97", 
-    "range=461.95", 
-    "range=497.92", 
-    "range=533.9", 
-    "range=569.87", 
-    "range=605.85", 
-    "range=641.82", 
-    "range=677.8", 
+elevation_angle = 77.5
+
+range_names = [
+    102.2, 
+    138.17, 
+    174.15, 
+    210.12, 
+    246.1, 
+    282.07, 
+    318.05, 
+    354.02, 
+    390.0, 
+    425.97, 
+    461.95, 
+    497.92, 
+    533.9, 
+    569.87, 
+    605.85, 
+    641.82, 
+    677.8 
 ]
-
 
 with h5py.File(file_name, 'r') as file:
     keys = list(file.keys())
@@ -34,6 +39,15 @@ with h5py.File(file_name, 'r') as file:
 
     data    = file.get('Data').get('Table Layout')
     dataset = np.array(data)
+
+
+    # Various altitudes
+    alt_names = []
+    for i in range(0, len(range_names)):
+        alt_names.append(calc_alt(6378.1, range_names[i], elevation_angle))
+
+    print('List of altitudes')
+    print(alt_names)
 
     # List entire dataset
     # print(dataset[5:])
@@ -53,12 +67,25 @@ with h5py.File(file_name, 'r') as file:
             ion_temp[altitudes].append(dataset[new_i+altitudes][21])
 
         new_i += 17
-    
-    for altitudes in range(0, len(ion_temp)):     
-        plt.plot(ion_temp[altitudes], label=alt_name[altitudes])
+
+
+    # Find peaks    
+    peaks, _ = find_peaks(ion_temp[0], height=0)
+
+
+    # Plot a graph
+    end_index = len(ion_temp)
+    for altitudes in range(0, end_index): # You can put the "end_index here"     
+        plt.plot(ion_temp[altitudes], label='Alt='+str(alt_names[altitudes]))
+
+    # Print peaks
+    print("Print peaks")
+    print(peaks)
+
+    plt.plot(peaks, np.array(ion_temp[0])[peaks], "x")
 
     plt.xlabel('Take each x increment in 15[min] intervals')
-    plt.ylabel('Ti, [K]')
+    plt.ylabel('Ti [K]')
     plt.show()
 
 
