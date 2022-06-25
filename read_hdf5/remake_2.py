65a8e5c21a7207fe5347ac8920a414d67dc3acfb
root <andygyakobo@gmail.com>
2022-06-25 11:17:08 -0400

    Used another method for peak detection

--- read_hdf5/remake_2.py
+++ read_hdf5/remake_2.py
@@ -1,8 +1,13 @@
 import h5py
+from numpy import NaN, Inf, arange, isscalar, asarray, array 
 import numpy as np
+import sys
 import matplotlib.pyplot as plt
 from   scipy.signal import find_peaks
 from   math import * 
+
+# EVIL Peak finding function
+from endolith_peakdet import *
 
 file_name = "data.hdf5"
 
@@ -14,27 +19,38 @@ def isNaN(num):
 
 
 
-
-
 def get_peaks(data, THRESHOLD=2):
     m_data = []
 
+    numb_of_nans = 0
+    
     for i in range(0, len(data)):
         if not isNaN(data[i]):
+            numb_of_nans += 1
             m_data.append(data[i])        
-
     data = m_data
+    
 
     mean    = sum(data) / len(data)
     std_dev = (sum( [ (val-mean)**2 for val in data ] )/ (len(data)-1))**0.5
 
-    peaks_x = []
+    peaks, plunges = peakdet(data, THRESHOLD*std_dev) 
+    
+    '''
     for i in range(1, len(data)-1):
         if (data[i] > data[i-1] and data[i] > data[i+1]):
             if (abs(data[i] - mean) > THRESHOLD*std_dev):
-                peaks_x.append(i)
+                peaks.append(i)
+    '''
 
-    return peaks_x, std_dev, mean
+    #peaks_x, _ = find_peaks(data, prominence=THRESHOLD*std_dev)
+
+    print('\n')
+    print('number of nans = ' + str(numb_of_nans) + '\n')
+
+    #type(peaks)
+
+    return array(peaks)[:,0], array(peaks)[:,1], array(plunges)[:,0], array(plunges)[:,1], std_dev, mean
 
 
 
@@ -99,18 +115,22 @@ with h5py.File(file_name, 'r') as file:
         ion_temp[i] = []
 
 
-    for i in range(0, 24*4):
-        for altitudes in range(0, 17):     
-            ion_temp[altitudes].append(dataset[new_i+altitudes][21])
+    hours_24 = 24
+    days = 7 
 
-        new_i += 17
+    for i in range(0, (hours_24*days) *4): #Needs to be 8
+        for altitudes in range(0, 17):     
+            #ion_temp[altitudes].append(dataset[new_i+altitudes][21])
+            ion_temp[altitudes].append(dataset[inc + (i+0)*17 + altitudes][21])
+
+        #new_i += 17
 
     # Choose your range
-    choose_range = 6
+    choose_range = 5 
 
     # Find peaks    
-    #peaks, _ = find_peaks(ion_temp[4], height=0)
-    peaks, std_dev, mean = get_peaks(ion_temp[choose_range])
+    # peaks, _ = find_peaks(ion_temp[choose_range], height=0)
+    peaks_x, peaks_y, plunges_x, plunges_y, std_dev, mean = get_peaks(ion_temp[choose_range], THRESHOLD=3)
 
     x = np.linspace(0, len(ion_temp[choose_range]))
 
@@ -123,9 +143,10 @@ with h5py.File(file_name, 'r') as file:
     # Print peaks
     print('\n')
     print("Print peaks")
-    print(peaks)
+    print(peaks_x)
 
-    plt.plot(peaks, np.array(ion_temp[choose_range])[peaks], "x")
+    plt.scatter(peaks_x, peaks_y, color="blue")
+    plt.scatter(plunges_x, plunges_y, color="red")
 
     # The standard deviation line 
     plt.plot([std_dev]*len(ion_temp[choose_range]), linestyle="--", label='Standard deviation = ' + str(std_dev)[:5])
