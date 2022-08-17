fd79c6496d9a8cf8bf354e924da2a1198305331c
Gyakobo <andygyakobo@gmail.com>
2022-08-17 04:08:13 -0400

    Test commit

--- New_API/Code-for-Andy-2.py
+++ New_API/Code-for-Andy-2.py
@@ -3,6 +3,7 @@
 
 
 # IMPORT THE REQUIRED LIBRARIES (DO NOT TOUCH, UNLESS ALTERING THE CODE)
+from random import sample
 import h5py as h5
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,8 +12,80 @@ import datetime
 import time
 
 
+
+
+
+
+
+
+# My Edits ################################################
+from numpy import NaN, Inf, arange, isscalar, asarray, array
+from math import *
+
+# EVIL Peak finding function
+from endolith_peakdet import *
+
+# Check whether the argument is a NaN
+def isNaN(num):
+  if float('-inf') < float(num) < float('inf'):
+    return False
+  else:
+    return True
+
+# Gets the peaks and the given plunges of a given array
+# Dr.Gareth asked to change the THRESHOLD
+def get_peaks(data, epochData, THRESHOLD = 2):
+  m_data = [] 
+
+  for item in data:
+    if not isNaN(item[0]):
+      m_data.append(item[0])
+  #data = m_data
+
+
+  mean    = sum(m_data) / len(m_data)
+  std_dev = (sum( [ (val-mean)**2 for val in m_data ] )/ (len(m_data)-1))**0.5
+  peaks, plunges = peakdet(m_data, THRESHOLD*std_dev)
+
+  peaks_x = []
+  plunges_x = []
+
+  #peaks_x   = [ (val, ) for val in array(peaks)[:,0] ] #Peaks per index
+  peaks_y   = [ (val, ) for val in array(peaks)[:,1] ]
+  for var in peaks_y:
+    index = data.index(var[0])
+    peaks_x.append(epochData[index]) 
+
+  ''' 
+  print('THESE ARE PEAKS ON THE X-AXIS')
+  print(np.array(peaks_x))
+  print('------------------------------------------')
+  print('THESE ARE PEAKS ON THE Y-AXIS')
+  print(np.array(peaks_y))
+  '''
+
+  #plunges_x   = [ (val, ) for val in array(plunges)[:,0] ] # Plunges per index
+  plunges_y   = [ (val, ) for val in array(plunges)[:,1] ]
+  for var in plunges_y:
+    index = data.index(var[0])
+    plunges_x.append(epochData[index]) 
+
+  return np.array(peaks_x), np.array(peaks_y), np.array(plunges_x), np.array(plunges_y), std_dev, mean
+  
+  #return array(peaks)[:,0], array(peaks)[:,1], array(plunges)[:,0], array(plunges)[:,1], std_dev, mean
+
+
+###########################################################
+
+
+
+
+
+
+
+
 # CUSTOMIZE THE PATH TO THE DATAFILE RELATIVE TO THE CODE
-path = "Andy PFISR/"
+path = "../../PFISR_data/"
 
 # INSERT THE NAME OF THE h5 DATAFILE YOU HAVE DOWNLOADED AND WOULD LIKE TO PLOT
 filename = "TIspike20220406.005_lp_1min-fitcal.h5"
@@ -97,9 +170,9 @@ ut = [datetime.datetime(int(time.gmtime((epochData[t,0]+epochData[t,1])/2)[0]), 
                        int(time.gmtime((epochData[t,0]+epochData[t,1])/2)[5])) # SECOND
                        for t in range(len(epochData))]
 
-
-
 # ERRONEOUS POINTS ARE REMOVED (COMMENT THIS OUT TO VIEW ALL MEASUREMENTS)
+
+'''
 for tt in range(len(nedata)):
   for ii in range(len(nedata[0])):
     for jj in range(len(nedata[0,0])):
@@ -109,6 +182,7 @@ for tt in range(len(nedata)):
         tidata[tt,ii,jj] = np.NaN
       if tedata[tt,ii,jj] < dtedata[tt,ii,jj]:
         tedata[tt,ii,jj] = np.NaN
+'''
 
 # WE CYCLE THROUGH THE DIFFERENT BEAMS IN THE EXPERIMENT
 for i in range(len(altdata)):
@@ -119,10 +193,34 @@ for i in range(len(altdata)):
     a = abs(altdata[i,:] - 275)
     b = np.where(a == np.nanmin(a))
     Ti275 = tidata[:,i,b[0]]
-    
+
+    # Still my edits ##################################
+    try:
+      peaks_x, peaks_y, plunges_x, plunges_y, std_dev, mean = get_peaks( Ti275.tolist(), epochData, THRESHOLD=3.5 )     
+      print(epochData[0][1] - epochData[0][0])
+
+    except:
+      print('Something went wrong')
+    ###################################################
+
+
     #WE PLOT TI
     plt.figure(figsize=(14, 7))
-    plt.plot(epochData,Ti275)
+    
+    plt.plot(epochData, Ti275) 
+    #plt.plot(Ti275)
+    ''' 
+    print('JUST EPOCHDATA')
+    print(epochData)
+    print('---------------------------------')
+    print('ACTUAL TI')
+    print(Ti275)
+    ''' 
+    
+    plt.plot(peaks_x, peaks_y, color="blue")
+    #plt.scatter(plunges_x, plunges_y, color="red")
+    
+    
     plt.title('Ti at 275 km')
     plt.grid(True)
     plt.xlabel('Epoch Time')
@@ -132,4 +230,5 @@ for i in range(len(altdata)):
     plt.cla()   # Clear axis
     plt.clf()   # Clear figure
     plt.close() # Close a figure window
+    #plt.show()
          
