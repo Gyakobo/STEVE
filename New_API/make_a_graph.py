e2d0d6a068ecb760f46052fa7356ba1463e1a4ce
Gyakobo <andygyakobo@gmail.com>
2023-01-26 23:29:06 -0500

    Filtering with Harsh's xlsx(s)

--- New_API/make_a_graph.py
+++ New_API/make_a_graph.py
@@ -2,20 +2,71 @@ import openpyxl
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker 
 import datetime
+import geopy.distance
 
+# Global Variable(s) and Function(s)
 path = '/home/andrew/STEVE/New_API/excel_files/Dataset_2016.xlsx'
+harsh_path_swarm_a = '/home/andrew/STEVE/New_API/Harsh_files_SWARM_A/currentYear2016-17.xlsx'
+harsh_path_swarm_b = '/home/andrew/STEVE/New_API/Harsh_files_SWARM_B/currentYear2016-17.xlsx'
+harsh_path_swarm_c = '/home/andrew/STEVE/New_API/Harsh_files_SWARM_C/currentYear2016-17.xlsx'
+
+poker_flat_lat  = 65.1200
+poker_flat_long = -147.4700 
+
+def curv_distance_km(lat1, long1, lat2, long2):
+    coords_1 = (lat1, long1)
+    coords_2 = (lat2, long2)
+    return geopy.distance.geodesic(coords_1, coords_2).km
+
+def curv_distance_miles(lat1, long1, lat2, long2):
+    coords_1 = (lat1, long1)
+    coords_2 = (lat2, long2)
+    return geopy.distance.geodesic(coords_1, coords_2).miles
 
 def EPOCH_to_DATE(epoch_time):
     date_conv = datetime.datetime.fromtimestamp(epoch_time)
     return date_conv.strftime("%d-%m, %H:%M:%S")
 
-def arrange_dates(x, y):
+def EPOCH_to_min(epoch_time):
+    date_conv = datetime.datetime.fromtimestamp(epoch_time)
+    return int(date_conv.strftime("%M"))
 
-    return None
+swarm_a_sim = {
+    'x': [],
+    'y': [],
+} 
+
+swarm_b_sim = {
+    'x': [],
+    'y': [],
+} 
+
+swarm_c_sim = {
+    'x': [],
+    'y': [],
+} 
+#################################
+
 
 # Open the workbook 
 wb_obj = openpyxl.load_workbook(path)
 sheet_obj = wb_obj.active
+
+wb_obj_swarm_a = openpyxl.load_workbook(harsh_path_swarm_a)
+sheet_obj_swarm_a = wb_obj_swarm_a.active
+wb_obj_swarm_b = openpyxl.load_workbook(harsh_path_swarm_b)
+sheet_obj_swarm_b = wb_obj_swarm_b.active
+wb_obj_swarm_c = openpyxl.load_workbook(harsh_path_swarm_c)
+sheet_obj_swarm_c = wb_obj_swarm_c.active
+
+'''
+for i in range(1, sheet_obj_swarm_a):
+    pass
+for i in range(1, sheet_obj_swarm_b):
+    pass
+for i in range(1, sheet_obj_swarm_c):
+    pass
+'''
 
 y = []
 x = []
@@ -23,8 +74,38 @@ numbs = []
 
 for i in range(1, sheet_obj.max_row): #1189
     # x.append(EPOCH_to_DATE(sheet_obj.cell(row=i+1, column=1).value))
-    x.append(sheet_obj.cell(row=i+1, column=1).value)
-    y.append(sheet_obj.cell(row=i+1, column=2).value)
+    x_slot = sheet_obj.cell(row=i+1, column=1).value 
+    y_slot = sheet_obj.cell(row=i+1, column=2).value 
+
+    for j in range(1, sheet_obj_swarm_a.max_row):
+        harsh_epoch_time = sheet_obj_swarm_a.cell(row=j+1, column=1).value.timestamp()
+        #harsh_te    = sheet_obj_swarm_a.cell(row=j+1, column=2).value
+        harsh_long  = sheet_obj_swarm_a.cell(row=j+1, column=3).value
+        harsh_lat   = sheet_obj_swarm_a.cell(row=j+1, column=4).value
+
+        if abs(int(x_slot) - int(harsh_epoch_time)) <= (30.0 * 60.0) and curv_distance_km(harsh_lat, harsh_long, poker_flat_lat, poker_flat_long) <= 500:
+            x.append(x_slot)
+            y.append(y_slot)
+    for j in range(1, sheet_obj_swarm_b.max_row):
+        harsh_epoch_time = sheet_obj_swarm_b.cell(row=j+1, column=1).value.timestamp()
+        #harsh_te    = sheet_obj_swarm_b.cell(row=j+1, column=2).value
+        harsh_long  = sheet_obj_swarm_b.cell(row=j+1, column=3).value
+        harsh_lat   = sheet_obj_swarm_b.cell(row=j+1, column=4).value
+
+        if abs(int(x_slot) - int(harsh_epoch_time)) <= (30.0 * 60.0) and curv_distance_km(harsh_lat, harsh_long, poker_flat_lat, poker_flat_long) <= 500:
+            x.append(x_slot)
+            y.append(y_slot)
+    for j in range(1, sheet_obj_swarm_c.max_row):
+        harsh_epoch_time = sheet_obj_swarm_c.cell(row=j+1, column=1).value.timestamp()
+        #harsh_te    = sheet_obj_swarm_a.cell(row=j+1, column=2).value
+        harsh_long  = sheet_obj_swarm_c.cell(row=j+1, column=3).value
+        harsh_lat   = sheet_obj_swarm_c.cell(row=j+1, column=4).value
+
+        if abs(int(x_slot) - int(harsh_epoch_time)) <= (30.0 * 60.0) and curv_distance_km(harsh_lat, harsh_long, poker_flat_lat, poker_flat_long) <= 500:
+            x.append(x_slot)
+            y.append(y_slot)
+
+
 
 for i in range(len(x)):
     numbs.append([x[i], y[i]])
@@ -59,7 +140,8 @@ plt.scatter(x_m, y_m, color="orange")
 plt.title('Ti at 275 km')
 # plt.grid(True)
 plt.subplots_adjust(bottom=0.162)
-plt.xticks(x_m[::15], rotation='vertical')
+#plt.xticks(x_m[::15], rotation='vertical')
+plt.xticks(x_m, rotation='vertical')
 
 # plt.axvline(x = 1476139380, color='r')
 
