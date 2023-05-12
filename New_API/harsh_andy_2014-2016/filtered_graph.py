1dc096fd3fe0b6115f766fbe3cfbc746d3a1e04f
Gyakobo <andygyakobo@gmail.com>
2023-05-12 00:09:21 -0400

    Finally found those 4 damn points

--- New_API/harsh_andy_2014-2016/filtered_graph.py
+++ New_API/harsh_andy_2014-2016/filtered_graph.py
@@ -2,34 +2,26 @@ import openpyxl
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker 
 import datetime
-import geopy.distance
-import os
 from openpyxl import Workbook
 import openpyxl
 
-
 # Global Variable(s) and Function(s)
-path_filtered      = './2014-2016_filtered.xlsx'
-
-# Main path
-path = path_filtered
+path    = '/home/andrew/STEVE/New_API/excel_files/filtered_ITET_data.xlsx'
 
 def EPOCH_to_DATE(epoch_time):
     date_conv = datetime.datetime.fromtimestamp(epoch_time)
     return date_conv
-
 
 def subtract_time(hours, minutes):
     time_in_minutes = hours*60 + minutes
 
     time_in_minutes -= (7*60+45)
 
-    if (time_in_minutes < 0): time_in_minutes += 24*60
+    if (time_in_minutes < 0):
+        time_in_minutes += 24*60
     
     hours   = time_in_minutes // 60
     minutes = time_in_minutes % 60
-
-    # print("hours:", hours, "minutes:", minutes)
 
     return hours, minutes
 ###################################################
@@ -40,50 +32,76 @@ sheet_obj = wb_obj.active
 
 y = []
 x = []
-
+name_of_file = []
 
 for i in range(1, sheet_obj.max_row): # sheet_obj.max_row
-    os.system('clear')
-    print("Iteration:", i, "/", sheet_obj.max_row)
     
-    date = EPOCH_to_DATE(sheet_obj.cell(row=i+1, column=2).value) 
-    y_slot = sheet_obj.cell(row=i+1, column=6).value 
+    date    = EPOCH_to_DATE(sheet_obj.cell(row=i+1, column=1).value) 
+    y_slot  = sheet_obj.cell(row=i+1, column=2).value 
+    
+    if y_slot >= 5000:
+        name_of_file.append(sheet_obj.cell(row=i+1, column=4).value[7:-3]) 
+    else: name_of_file.append(None) 
 
     if (y_slot <= 20000):        
         year    = date.year 
         time    = date.time()
  
         hours, minutes = subtract_time(time.hour, time.minute)
-        alaska_datetime = datetime.datetime(
-                year=2000, month=1, day=1, hour=hours, minute=minutes,
-                second=time.second, microsecond=time.microsecond) 
- 
-        x.append(alaska_datetime)
+        x.append(hours + minutes / 60.0)
         y.append(y_slot)
 
 
-pre_noon        = datetime.datetime(2000, 1, 1, 0, 0, 0)
-post_midnight   = datetime.datetime(2000, 1, 1, 6, 0, 0)
-post_noon       = datetime.datetime(2000, 1, 1, 12, 0, 0)
-pre_midnight    = datetime.datetime(2000, 1, 1, 18, 0, 0)
-day_time = [ post_midnight, pre_noon, post_noon, pre_midnight ]
+pre_noon        = 0
+post_midnight   = 6
+post_noon       = 12
+pre_midnight    = 18
+night           = 24 
 
-plt.figure(figsize=(14, 9))
+day_time = [ post_midnight, pre_noon, post_noon, pre_midnight, night ]
 
-plt.scatter(x, y, color="orange", label="IT: " + str(len(y)))
+fig = plt.figure(figsize=(14, 10))
 
-plt.grid(True)
+# Bins for the histograms
+bins = []
+i = 0 
+while(i <= 24):
+    bins.append(i)
+    i += 0.5 
 
-plt.title('Ti at 275 km')
-plt.subplots_adjust(bottom=0.162)
+# Scatter plot
+# ax1 = fig.add_subplot(211)
+ax1 = fig.add_subplot(111)
 
+ax1.scatter(x, y, color="orange", label="IT: " + str(len(y)))
+
+for i, txt in enumerate(name_of_file):
+    ax1.annotate(txt, (x[i], y[i]))
+
+ax1.grid(True)
+ax1.set_title('Ti at 275 km')
+ax1.legend()
+
+# Histogram plot #1
+# ax2 = fig.add_subplot(212)
+# ax2.hist(x, bins=bins, edgecolor='black')
 for i in day_time: 
-    plt.axvline(x = i, color='r')
+    ax1.axvline(x = i, color='r')
+    # ax2.axvline(x = i, color='r')
 
-plt.xlabel("IT: " + str(sheet_obj.max_row) + " points found")
-plt.ylabel('Line-of-Sight Ion Temperature [K]')
-
-plt.legend()
 plt.plot()
-
 plt.show()
+
+############################### Deprecated ##################################
+
+# plt.scatter(x, y, color="orange", label="IT: " + str(len(y)))
+# plt.grid(True)
+# plt.title('Ti at 275 km')
+# plt.subplots_adjust(bottom=0.162)
+
+# for i in day_time: 
+#    plt.axvline(x = i, color='r')
+
+# plt.xlabel("IT: " + str(sheet_obj.max_row) + " points found")
+# plt.ylabel('Line-of-Sight Ion Temperature [K]')
+# plt.legend()
--- New_API/harsh_andy_2014-2016/unfiltered_graph.py
+++ New_API/harsh_andy_2014-2016/unfiltered_graph.py
@@ -2,11 +2,8 @@ import openpyxl
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker 
 import datetime
-# import geopy.distance
-# import os
 from openpyxl import Workbook
 import openpyxl
-import numpy as np
 
 
 # Global Variable(s) and Function(s)
@@ -36,19 +33,22 @@ sheet_obj = wb_obj.active
 
 y = []
 x = []
-
+name_of_file = []
+n_bin_del = 0.5
+n = [0]*24
 
 for i in range(1, sheet_obj.max_row): # sheet_obj.max_row
-    # os.system('clear')
-    # print("Iteration:", i, "/", sheet_obj.max_row)
     
     date    = EPOCH_to_DATE(sheet_obj.cell(row=i+1, column=1).value) 
     y_slot  = sheet_obj.cell(row=i+1, column=2).value 
 
+    name_of_file.append(sheet_obj.cell(row=i+1, column=6).value)
+    # TIspike20150529.002_lp_5min.h5
+
     if (y_slot <= 20000):        
-        year    = date.year #sheet_obj_swarm_a.cell(row=j+1, column=1).value
+        year    = date.year
         time    = date.time()
- 
+
         hours, minutes = subtract_time(time.hour, time.minute)
         x.append(hours + minutes / 60.0)
         y.append(y_slot)
@@ -69,10 +69,11 @@ bins = []
 i = 0 
 while(i <= 24):
     bins.append(i)
-    i += 0.5 
+    i += n_bin_del
 
 # Scatter plot
 ax1 = fig.add_subplot(211)
+# ax1 = fig.add_subplot(111)
 ax1.scatter(x, y, color="orange", label="IT: " + str(len(y)))
 ax1.grid(True)
 ax1.set_title('Ti at 275 km')
